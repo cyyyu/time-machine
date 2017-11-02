e2448bb428b37774553a541a67288076169c0ba6
cyyyu <cyu9960@gmail.com>
2017-11-02 11:27:25 +0800

    time machine prototype

---
+++ src/timeMachine.ts
@@ -0,0 +1,38 @@
+import {
+  makeList,
+  makePair,
+  car,
+  cdr,
+  filter,
+  ref,
+} from './core'
+import { fromJS } from 'immutable'
+
+type year = number
+interface passager {
+  name: string
+  state: any
+}
+
+// prototype
+class TimeMachine {
+  private _passagers = fromJS({})
+  private _states = fromJS({})
+  private _v(state, fn = (state, allStates) => state) {
+    return lastState => makePair(state, () => this._v(fn(state, lastState), fn)(lastState))
+  }
+
+  public onBoard = (name: passager['name'], state: passager['state'], fn?: (state, allStates?) => any): TimeMachine => {
+    if (this._passagers.get(name)) throw new Error('Passager is already onboard.')
+    this._passagers = this._passagers.set(name, this._v(fromJS(state), fn))
+    this._states = this.travel(0)
+    return this
+  }
+
+  public travel = (num: year, tmp = this._states) => {
+    const newState = this._passagers.map(i => ref(i(tmp), num))
+    return newState.equals(tmp) ? newState : this.travel(num, newState)
+  }
+}
+
+export default TimeMachine  
---
+++ test/timeMachine.test.ts
@@ -0,0 +1,52 @@
+import {
+  expect
+} from 'chai'
+import TimeMachine from '../src/timeMachine'
+import { fromJS } from 'immutable'
+
+describe('TimeMachine', () => {
+
+  it('add passagers & travel', () => {
+    const machine = new TimeMachine()
+
+    const father = {
+      age: 34,
+      alive: true,
+      feeling: 'happy'
+    }
+    const daughter = {
+      age: 7,
+      alive: true,
+      feeling: 'happy'
+    }
+
+    machine
+      .onBoard('father', father, (state) => {
+        let newState = state
+        if (state.get('age') < 89) {
+          newState = state.set('age', state.get('age') + 1) // age grows by years
+        }
+        if (newState.get('age') > 88) {
+          newState = newState.set('alive', false) // dead at 89
+        }
+        return newState
+      })
+      .onBoard('daughter', daughter, (state, allStates) => {
+        let newState = state
+        const fatherAlive = allStates.getIn(['father', 'alive'])
+        if (!fatherAlive) {
+          newState = state.set('feeling', 'sad')
+        }
+        newState = newState.set('age', newState.get('age') + 1)
+        return newState
+      })
+
+    expect(machine.travel(1).getIn(['father', 'age'])).to.equal(35)
+    expect(machine.travel(30).getIn(['father', 'age'])).to.equal(64)
+    expect(machine.travel(55).getIn(['father', 'alive'])).to.equal(false)
+    expect(machine.travel(55).getIn(['daughter', 'feeling'])).to.equal('sad')
+    expect(machine.travel(100).getIn(['father', 'age'])).to.equal(89)
+    expect(machine.travel(100).getIn(['father', 'age'])).to.equal(89)
+  })
+
+})
